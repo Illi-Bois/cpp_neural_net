bd89cb97b694664eaef12c69bab6555bd763dda5
Brian Kim <160251900+briankim0629@users.noreply.github.com>
2024-06-08 00:16:26 +0900

    constructor linear c--

--- src/CPPNeuralNet/layers.cpp
+++ src/CPPNeuralNet/layers.cpp
@@ -1,6 +1,15 @@
-#include "include/CPPNeuralNet/layers.h"
+#include "include/CPPNeuralNet/layer.h"
 #include "include/CPPNeuralNet/Utils/utils.h"
+#include "../../include/CPPNeuralNet/layer.h"
 
+Linear::Linear(int in_features, int out_features, double lr)
+    : weights_(out_features, in_features, 0.0),
+      biases_(out_features, 0.0),
+      learning_rate(lr) {
+        //Initialize weights and biases with random values 
+        //my include is acting weird again so there might be typos ;( will fix later)
+
+} 
 
 
 util::Vector<double> Linear::forward(util::Vector<double> input){
