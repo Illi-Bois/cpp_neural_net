367c9a1e7e0b7c20ff40795137651b168c25a655
Brian Kim <160251900+briankim0629@users.noreply.github.com>
2024-06-10 00:34:15 +0900

    Linear Constructor v1

--- src/CPPNeuralNet/layers.cpp
+++ src/CPPNeuralNet/layers.cpp
@@ -1,13 +1,25 @@
 #include "include/CPPNeuralNet/layer.h"
 #include "include/CPPNeuralNet/Utils/utils.h"
-#include "../../include/CPPNeuralNet/layer.h"
+#include <random>
 
 Linear::Linear(int in_features, int out_features, double lr)
     : weights_(out_features, in_features, 0.0),
       biases_(out_features, 0.0),
-      learning_rate(lr) {
+      learning_rate_(lr) {
+    std::random_device rand_num;
+    std::mt19937 generate_rand(rand_num);
+    // got random number generate code online. random = object of std::random, gets random number from hardware random number generator
+    // random (creates seed number) -> std::mt19937 -> better random number apparantly
+    std::normal_distribution<double> dis(0.0,0.01);
+    for(int i = 0; i < out_features; ++i){
+      for(int j = 0; j < in_features; ++j){
+        weights_(i,j) = dis(generate_rand);
+      }
+      biases_(i) = dis(generate_rand);
+    }
+
+
         //Initialize weights and biases with random values 
-        //my include is acting weird again so there might be typos ;( will fix later)
 
 } 
 
