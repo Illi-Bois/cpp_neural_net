043fcff8714280cf757a942f938d3151886d76ee
jack <sungjai2@illinois.edu>
2024-06-12 23:04:38 +0900

    Some ideas written up in util.h

--- include/CPPNeuralNet/Utils/utils.h
+++ include/CPPNeuralNet/Utils/utils.h
@@ -205,3 +205,34 @@ class Tensor{
 
 
 #endif  // CPP_NN_UTIL
+
+
+
+/**
+ * Some Planning:
+ * 
+ * 
+ * Essential Functionalities:
+ * - MatMul
+ * - MatSum
+ * - Transpose
+ * 
+ * ** Often, tranpose only appears when multiplcation or other operation is to follow it immediately
+ * *** as such, it may be more beneficial/effective if tranpose is done as 'Flag' rather than
+ *      Element movement
+ * 
+ * -> suggests, of course, need for MatMul with Transpose?
+ * -> or simply change in accessor is enough
+ * 
+ * - pros for TransposeMatMul
+ * allows quick and light weight transpose transitions
+ * for temperory transposing, effectively no memories are moved
+ * 
+ * - cons
+ * no quick in place operation
+ * ie, we need to define separate operation like *_tranposed to use in line
+ * else we will have to keep track of when and where transpose were called
+ * 
+ * 
+ * This whole issue and more may be averted by forming a more structured Tensor definition.
+ */
