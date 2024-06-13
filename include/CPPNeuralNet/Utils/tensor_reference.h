e8398099c50463acbe0fa255ee859523701203e3
jack <sungjai2@illinois.edu>
2024-06-13 23:28:09 +0900

    TensorReference, a generalization of MatrixReference's signitures added

--- include/CPPNeuralNet/Utils/tensor_reference.h
+++ include/CPPNeuralNet/Utils/tensor_reference.h
@@ -8,6 +8,54 @@
 
 namespace cpp_nn {
 namespace util {
+
+/**
+ * TensorReference.
+ * A lightweight accessor and iterator over Tensor. 
+ * Given a Tensor and order of TensorChunk, ie 2 for Matrix,
+ *    Will provide accessors and iterators over the multiarray of Tensors.
+ */
+template <typename T, int kChunkOrder>
+class TensorReference { // ================================================================================
+ private:
+// Members ------------------------------------------------------
+  Tensor<T>::TensorElement* elements_; // ownership is never given
+  std::vector<int> index_; // order = order(Tensor) - kChunkOrder
+// End of Members -----------------------------------------------
+
+ public:
+// Constructor --------------------------------------------------
+/** Tensor-Referencing
+ *  Index is set to 0th, or very first matrix. */
+  TensorReference(Tensor<T>& tensor);
+/** Tensor-Referencing with Index
+ *  Index is set as specified. 
+ *    Throws error for 
+ *      'Order Mismatch'
+ *      'Index out of Bounds' */
+  TensorReference(Tensor<T>& tensor, std::initializer_list<int> indices);
+// End of Constructor -------------------------------------------
+
+
+// Accessors ----------------------------------------------------
+/** Getter
+ *  Retrieves element at index of current TensorChunk
+ */
+  T& getElement(std::vector<int> index);
+/** Getter Parenthesis Notation */
+  inline T& operator()(std::vector<int> index) {return getElement(index);}
+// End of Accessors ---------------------------------------------
+
+// Iteration ----------------------------------------------------
+/** increments one matrix over. 
+ *  Returns 1 for successful incrementation, 0 for failed incrementation.
+ * 
+ * After failure, is set to 0th index again. Therefore checking terminatin with return flag is crucial.
+ */
+  int incrementIndex();
+// End of Iteration ---------------------------------------------
+}; // End of TensorReference ==============================================================================
+
 /**
  * Intermediary object to aid in referencing matrices in a Tensor. 
  * MatrixReference will allow easy access into given Tensor's lowest level matrices
