dca3198abe14b3b979803d34ff7ea4e7f1b06440
jack <sungjai2@illinois.edu>
2024-07-08 17:09:41 +0900

    Different testcase names and edge cases thought of for tensor_operation

---
+++ tests/util_tensor_operation_test.cpp
@@ -0,0 +1,120 @@
+#include "gtest/gtest.h"
+
+#include "CPPNeuralNet/Utils/re_tensor.h"
+
+TEST(UtilTensorOperation, SanityCheck) {
+  EXPECT_TRUE(true);
+}
+
+// TRANSPOSE =============================================
+TEST(UtilTensorOperation, Transpose_Operation) {
+  
+}
+TEST(UtilTensorOperation, Undoing_Transpose) {
+  
+}
+TEST(UtilTensorOperation, Chained_Transpose) {
+  
+}
+TEST(UtilTensorOperation, Negative_Indexing) {
+  
+}
+TEST(UtilTensorOperation, Transpose_Self) {
+  
+}
+// End of TRANSPOSE ======================================
+
+// SUMMATION =============================================
+TEST(UtilTensorOperation, Summation_Of_Two) {
+  
+}
+TEST(UtilTensorOperation, Summation_Of_Three) {
+  
+}
+TEST(UtilTensorOperation, Summation_Of_many_with_parenthesis) {
+  
+}
+TEST(UtilTensorOperation, SelfSumming) {
+  
+}
+TEST(UtilTensorOperation, Summing_To_Self) {
+  
+}
+TEST(UtilTensorOperation, Summing_To_Self_with_others_inbetween) {
+  
+}
+TEST(UtilTensorOperation, Incorrect_Dimensions) {
+  
+}
+// End of SUMMATION ======================================
+
+// MULTIPLICATION ========================================
+TEST(UtilTensorOperation, Multiplication_of_two) {
+  
+}
+TEST(UtilTensorOperation, Multiplication_of_three) {
+  
+}
+TEST(UtilTensorOperation, Multiplication_of_many_with_parenthesis) {
+  
+}
+TEST(UtilTensorOperation, Multiplication_to_self) {
+  
+}
+TEST(UtilTensorOperation, Multiplication_of_many_involving_to_self) {
+  
+}
+TEST(UtilTensorOperation, Incorrect_matrix_dim) {
+  
+}
+TEST(UtilTensorOperation, Incorrect_tensor_dim) {
+  
+}
+// END OF MULTIPLICATION =================================
+
+// RESHAPE ===============================================
+TEST(UtilTensorOperation, Rehsaping) {
+  
+}
+TEST(UtilTensorOperation, Rehsaping_to_self) {
+  
+}
+TEST(UtilTensorOperation, Reshaping_chained) {
+  
+}
+TEST(UtilTensorOperation, Reshape_incorrect_capacity) {
+  
+}
+TEST(UtilTensorOperation, Reshape_with_non_post_dim) {
+  
+}
+// END OF RESHAPE ========================================
+
+// PADDING ===============================================
+TEST(UtilTensorOperation, Padding) {
+  
+}
+TEST(UtilTensorOperation, Padding_to_self) {
+  
+}
+TEST(UtilTensorOperation, Multiple_Padding) {
+  
+}
+TEST(UtilTensorOperation, Reductive_padding) {
+  
+}
+TEST(UtilTensorOperation, Padding_incorrect_order) {
+  
+}
+TEST(UtilTensorOperation, Padding_non_postive_dim) {
+  
+}
+// END OF PADDING ========================================
+
+
+
+// MULTIPLE OPERATION CHAINED ============================
+TEST(UtilTensorOperation, Chaining_Different_Operations) {
+  // do while and go ham, 
+}
+// End of MULTIPLE OPERATION CHAINED =====================
